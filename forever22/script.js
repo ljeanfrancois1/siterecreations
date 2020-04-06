e267087116ca4f1770585c62832b364fe05b2212
Leah Jean-Francois <leahjf98@gmail.com>
2020-04-05 22:54:53 -0400

    forever22

---
+++ forever22/script.js
@@ -0,0 +1,9 @@
+
+   function toggleSearchDiv() {
+    var x = document.getElementById("search-div");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  } 
